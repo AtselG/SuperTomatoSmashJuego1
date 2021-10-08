8e4f5cb09bf2e50c5a30c5ab3cc788c1e3e4a615
elatsel <80478653+elatsel@users.noreply.github.com>
2021-10-08 20:01:09 -0300

    4283d73a

--- SuperTomatoSmashJuego1/Info/infoGanar.vb
+++ SuperTomatoSmashJuego1/Info/infoGanar.vb
@@ -1,7 +1,8 @@
 ﻿Public Class InfoGanar
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Close()
         FormInicio.Show()
+        Close()
+        FormJugar.Close()
     End Sub
     Private Sub Info2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.Icon = (My.Resources.Icono)
